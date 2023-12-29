87ec4ed7343eb692bc82b0b969053379e2e4a872
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-12-28 19:10:21 -0800

    changed users_controller user create to User.new and removed other attributes and added password_confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,20 +5,11 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.create(
+    user = User.new(
       user_name: params[:user_name],
-      user_fname: params[:user_fname],
-      user_lname: params[:user_lname],
       user_email: params[:user_email],
       password: params[:password],
-      user_phn_num: params[:user_phn_num],
-      user_bio: params[:user_bio],
-      user_linkedin_url: params[:user_linkedin_url],
-      user_twitter_handle: params[:user_twitter_handle],
-      user_website_url: params[:user_website_url],
-      user_resume_url: params[:user_resume_url],
-      user_github_url: params[:user_github_url],
-      user_photo_url: params[:user_photo_url],
+      password_confirmation: params[:password_confirmation],
     )
     if user.save
       render json: { message: "User created successfully" }, status: :created
